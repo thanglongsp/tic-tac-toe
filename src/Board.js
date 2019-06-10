939dd59625eb900abee12121a7141c23420f40f5
Nguyễn Thăng Long <longnt8@topica.edu.vn>
2019-06-10 17:24:18 +0700

    completed task 2 (1,2)

--- src/Board.js
+++ src/Board.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
-    renderSquare(i) {
+    renderSquare(i){
         return (
             <Square
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
+                id={ i }
             />
         );
     }
@@ -18,16 +19,36 @@ class Board extends React.Component {
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
                     {this.renderSquare(3)}
                     {this.renderSquare(4)}
-                    {this.renderSquare(5)}
                 </div>
                 <div className="board-row">
+                    {this.renderSquare(5)}
                     {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
+                    {this.renderSquare(9)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(10)}
+                    {this.renderSquare(11)}
+                    {this.renderSquare(12)}
+                    {this.renderSquare(13)}
+                    {this.renderSquare(14)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(15)}
+                    {this.renderSquare(16)}
+                    {this.renderSquare(17)}
+                    {this.renderSquare(18)}
+                    {this.renderSquare(19)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(20)}
+                    {this.renderSquare(21)}
+                    {this.renderSquare(22)}
+                    {this.renderSquare(23)}
+                    {this.renderSquare(24)}
                 </div>
             </div>
         );
--- src/Square.js
+++ src/Square.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './Css.css';
 
 function Square(props) {
-    return (
-        <button className="square" onClick={props.onClick}>
+    let button;
+    console.log(props);
+    if( props.id % 2 === 0) {
+        button = <button className="square1" onClick={props.onClick}>
             {props.value}
         </button>
+    }
+    else {
+        button = <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    }
+    return (
+        button
     );
 }
 
